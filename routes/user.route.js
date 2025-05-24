f35acf5edfb3c4005cd1fdd28336bc2e7b812bd4
prakshal390 <sj8104669@gmail.com>
2025-05-24 21:19:40 +0530

    c

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,18 +1,10 @@
 import express from 'express';
 import { login, logout, register } from '../controllers/user.controller.js';
 
-const router = express.Router();                                                 //initialize the express router
-router.route('/register').post(register);                                        //create the register route with the post request
-router.route('/login').post(login);                                              //create the login route with the post request
-router.route('/logout').get(logout);                                             //create the logout route with the get request
+const router = express.Router(); //initialize the express router
 
-res.cookie("token", token, {
-  httpOnly: true,
-  secure: true, // must be true for cross-site cookies on HTTPS
-  sameSite: "none", // must be "none" for cross-site cookies
-  // domain: ".onrender.com", // optional: only if both frontend and backend are on the same parent domain
-  maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
-});
-
+router.route('/register').post(register); //create the register route with the post request
+router.route('/login').post(login); //create the login route with the post request
+router.route('/logout').get(logout); //create the logout route with the get request
 
 export default router;
