75150a80a56e85eb4653b0accd89fc39795cf52a
prakshal390 <sj8104669@gmail.com>
2025-05-24 21:15:14 +0530

    some changes

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,5 +6,13 @@ router.route('/register').post(register);                                       
 router.route('/login').post(login);                                              //create the login route with the post request
 router.route('/logout').get(logout);                                             //create the logout route with the get request
 
+res.cookie("token", token, {
+  httpOnly: true,
+  secure: true, // must be true for cross-site cookies on HTTPS
+  sameSite: "none", // must be "none" for cross-site cookies
+  // domain: ".onrender.com", // optional: only if both frontend and backend are on the same parent domain
+  maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
+});
+
 
 export default router;
